3422887a4917b2f340a077d5e8528e263ae05da9
Arafath3 <arafathreja0@gmail.com>
2024-06-30 00:27:14 +1000

    finished

--- main.py
+++ main.py
@@ -1,30 +1,51 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+from scoreboard import ScoreBoard
+import time
 
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Ping Pong Game")
+screen.listen()
+screen.tracer(0)
 
-paddle = Paddle()
+score = ScoreBoard()
+r_paddle = Paddle((380, 0))
+l_paddle = Paddle((-380, 0))
+ball = Ball()
 
+screen.onkey(r_paddle.up, "Up")
+screen.onkey(r_paddle.down, "Down")
 
+screen.onkey(l_paddle.up, "w")
+screen.onkey(l_paddle.down, "s")
 
+game_is_on = True
 
+speed = 5
 
+while game_is_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+    # Detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
 
+    # Detect collision with right paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+        ball.speed("fast")
 
+    # Detect collision with wall if missed
+    if ball.xcor() > 380:
+        ball.reset()
+        score.l_points()
 
-
-
-
-
-
-
-
-
-
-
-
+    if ball.xcor() < - 380:
+        ball.reset()
+        score.r_points()
 
 screen.exitonclick()
--- paddle.py
+++ paddle.py
@@ -1,19 +1,22 @@
 from turtle import Turtle
 
-POSITION = [(380, 0), (-380, 0)]
-
 
 class Paddle(Turtle):
-    def __init__(self):
+    def __init__(self, tup):
         super().__init__()
-        self.paddles = []
-        self.create_paddle()
+        self.shape("square")
+        self.shapesize(stretch_wid=4, stretch_len=.7)
+        self.penup()
+        self.color("white")
+        self.goto(tup)
 
-    def create_paddle(self):
-        for paddle in POSITION:
-            new_paddle = Turtle("square")
-            new_paddle.shapesize(stretch_wid=4, stretch_len=.7)
-            new_paddle.penup()
-            new_paddle.color("white")
-            new_paddle.goto(paddle)
-            self.paddles.append(new_paddle)
+    def up(self):
+        y_new = self.ycor() + 20
+        self.goto(self.xcor(), y_new)
+
+    def down(self):
+        y_new = self.ycor() - 20
+        self.goto(self.xcor(), y_new)
+
+
+
